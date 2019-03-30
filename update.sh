40b78ff0fbca1d574e87fb01c11247ba255a3d32
Joy Ghosh <36255129+JoyGhoshs@users.noreply.github.com>
2019-03-30 10:04:16 -0400

    Utools

---
+++ update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#Project Utools2
+#Project From Joy JoyGhosh
+#Joychondroghosh@gmail.com
+echo """
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+UTOOLS2 UPDATE    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+"""
+rm utools
+wget https://raw.githubusercontent.com/JoyGhoshs/Utools2/master/utools
+chmod +x utools
+cd modules
+rm brutef
+rm crunch
+rm ddos
+rm fbbrute
+rm hamm
+rm info
+rm instabr
+rm metasp
+rm payload
+rm phoneinfoga
+rm webpen
+rm webshell
+rm wgen
+rm wireless
+rm wordlist
+wget https://raw.githubusercontent.com/JoyGhoshs/Utools2/master/modules/brutef
+wget https://raw.githubusercontent.com/JoyGhoshs/Utools2/master/modules/crunch
+wget https://raw.githubusercontent.com/JoyGhoshs/Utools2/master/modules/ddos
+wget https://raw.githubusercontent.com/JoyGhoshs/Utools2/master/modules/fbbrute
+wget https://raw.githubusercontent.com/JoyGhoshs/Utools2/master/modules/hamm
+wget https://raw.githubusercontent.com/JoyGhoshs/Utools2/master/modules/info
+wget https://raw.githubusercontent.com/JoyGhoshs/Utools2/master/modules/instabr
+wget https://raw.githubusercontent.com/JoyGhoshs/Utools2/master/modules/metasp
+wget https://raw.githubusercontent.com/JoyGhoshs/Utools2/master/modules/payload
+wget https://raw.githubusercontent.com/JoyGhoshs/Utools2/master/modules/phoneinfoga
+wget https://raw.githubusercontent.com/JoyGhoshs/Utools2/master/modules/webpen
+wget https://raw.githubusercontent.com/JoyGhoshs/Utools2/master/modules/webshell
+wget https://raw.githubusercontent.com/JoyGhoshs/Utools2/master/modules/wgen
+wget https://raw.githubusercontent.com/JoyGhoshs/Utools2/master/modules/wireless
+wget https://raw.githubusercontent.com/JoyGhoshs/Utools2/master/modules/wordlist
+
+read -p "UpDated)"
